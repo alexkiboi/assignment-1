4d39f3e57aa5395a27dbc9bca61c56e02cffcfde
alexkiboi <alexkimondo20@gmail.com>
2026-06-11 11:44:36 +0300

    Fix syntax errors and update comments in assessment

--- javascript fundamentals part 1/assessment (1).js
+++ javascript fundamentals part 1/assessment (1).js
@@ -1,4 +1,4 @@
-'use strict';
+
 // LECTURE: Values and Variables
 
 /* 
@@ -103,11 +103,11 @@ const halfPopulation = population / 2 ;
 // /*
 // LECTURE: Type Conversion and Coercion
 // 1. Predict the result of these 5 operations without executing them:
-// '9' - '5';
-// '19' - '13' + '17';
- //'19' - '13' + 17;
-//'123' < 57;
-// 5 + 6 + '4' + 9 - 4 - 2;
+ '9' - '5';
+ '19' - '13' + '17';
+ '19' - '13' + 17;
+'123' < 57;
+ 5 + 6 + '4' + 9 - 4 - 2;
 // 2. Execute the operations to check if you were right
 */ 
 
